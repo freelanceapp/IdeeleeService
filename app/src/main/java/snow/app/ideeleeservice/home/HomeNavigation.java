17da823807fb2997ac5af057bfd93d97475aa933
snow-dell-07 <Tarun.Mehandiratta10@gmail.com>
2019-05-30 18:30:13 +0530

    commit by simar

--- app/src/main/java/snow/app/ideeleeservice/home/HomeNavigation.java
+++ app/src/main/java/snow/app/ideeleeservice/home/HomeNavigation.java
@@ -85,6 +85,7 @@ public class HomeNavigation extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
